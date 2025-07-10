0e3696816b2ae6d0b346eecdcd8dcade8bdd0bfa
Chris <c.schrijver2@gmail.com>
2025-07-10 19:40:59 +0200

    ewrwer

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,13 +1,6 @@
 import type { LoaderFunctionArgs } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
+import { login } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
-
-  return new Response(null, {
-    status: 302,
-    headers: {
-      Location: "/app",
-    },
-  });
+  return await login(request);
 }; 
