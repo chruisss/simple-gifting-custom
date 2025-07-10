76a9277249db56246b05429d32899b8ed72b9db0
Chris <c.schrijver2@gmail.com>
2025-07-10 20:50:42 +0200

    bjn

--- app/routes/auth.$.tsx
+++ app/routes/auth.$.tsx
@@ -1,7 +1,15 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { login } from "../shopify.server";
+import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { authenticate, login } from "../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await login(request);
+  // Handles the OAuth callback from Shopify.
+  // authenticate.admin will internally handle the redirect.
+  await authenticate.admin(request);
+
   return null;
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+  // Handles the form submission from the index page to initiate OAuth.
+  return login(request);
 }; 
