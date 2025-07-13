7cdbbfc12fdb40201f804573a732ef38ae77ffe0
Chris <c.schrijver2@gmail.com>
2025-07-13 11:09:07 +0200

    43ter

--- app/routes/auth.login/route.tsx
+++ app/routes/auth.login/route.tsx
@@ -1,6 +1,10 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
+import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
 import { login } from "../../shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
+  return login(request);
+};
+
+export const action = async ({ request }: ActionFunctionArgs) => {
   return login(request);
 };
